c1c0ff5a32d11593a7a28f13dc11dceb8c0584b7
ameya-6964 <belvalkar7@gmail.com>
2024-02-19 21:45:56 +0530

    Added Signature Of The Method

--- Basics-Challenge-Part-1/ReverseString.js
+++ Basics-Challenge-Part-1/ReverseString.js
@@ -1,3 +1,10 @@
+/**
+ * Returns the reverse of a string.
+ * @param {string} str - The string to reverse.
+ * @returns {string} - The reverse of the string.
+  function reverseString(str: string): string;
+ */
+
 function reverseString(string) {
   let word = string.split("");
   let reversed = "";
