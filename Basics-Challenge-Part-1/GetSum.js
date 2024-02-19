92230169c0c2482bf39cd07f87b46d09c951bd35
ameya-6964 <belvalkar7@gmail.com>
2024-02-19 21:20:15 +0530

    Added getSum Function Challenge

---
+++ Basics-Challenge-Part-1/GetSum.js
@@ -0,0 +1,14 @@
+/**
+ * Returns the sum of two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} - The sum of the two numbers.
+   function getSum(a: number, b: number): number;
+ */
+
+function getSum(a, b) {
+  return a + b;
+}
+
+const result = getSum(2, 3); //5
+console.log(result);
